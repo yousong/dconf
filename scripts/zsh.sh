4a9f29008f238dfc4ccb32742e389fb091035a74
Yousong Zhou <yszhou4tech@gmail.com>
2015-05-27 00:55:03 +0800

    zsh.sh: apply patch with `git am' on `dconf' branch.

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -7,11 +7,12 @@ ohmyzsh_dir="$HOME/.oh-my-zsh"
 config() {
 	[ -d "$ohmyzsh_dir" ] || {
 		git clone https://github.com/robbyrussell/oh-my-zsh.git "$ohmyzsh_dir"
-		cd "$ohmyzsh_dir"
-		for f in "$PATCH_DIR/_oh-my-zsh"/*; do
-			patch -i "$f" -p1
-		done
 	}
+
+	cd "$ohmyzsh_dir"
+	git checkout -b dconf >/dev/null 2>&1 || true
+	git reset --hard master
+	git am "$PATCH_DIR/_oh-my-zsh"/*
 
 	cp "$DATA_DIR/_zshrc" "$HOME/.zshrc"
 	__errmsg "zsh: use 'chsh -s /bin/zsh' to change shell."
