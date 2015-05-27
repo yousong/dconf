df014d9ca127637b1e128d5c414abbf1f191805d
Yousong Zhou <yszhou4tech@gmail.com>
2015-05-27 10:37:50 +0800

    vim.sh: allow applying patches to vim plugins.

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -2,9 +2,12 @@
 
 . "$TOPDIR/env.sh"
 
-vundle_repo="$HOME/.vim/bundle/Vundle.vim"
+bundle_dir="$HOME/.vim/bundle"
 
 config() {
+	local vundle_repo="$bundle_dir/Vundle.vim"
+	local plugin d
+
 	if [ ! -d "$vundle_repo/.git" ]; then
 		git clone https://github.com/gmarik/Vundle.vim.git "$vundle_repo"
 	fi
@@ -13,6 +16,16 @@ config() {
 
 	__errmsg "vim: Installing Vundle packages, this may take quite a while."
 	vim +BundleInstall +qa
+
+	for plugin in $(find "$PATCH_DIR/_vim" -depth 2 -type d); do
+		d="$bundle_dir/$(basename "$plugin")"
+		if [ -d "$d" ]; then
+			cd "$d"
+			git checkout -b dconf >/dev/null 2>&1 || true
+			git reset --hard master
+			git am "$plugin"/*
+		fi
+	done
 }
 
 collect() {
