8cfb5e2bcbcd4b710ab6f31ad81dcff97286b59c
Yousong Zhou <yszhou4tech@gmail.com>
2015-11-16 17:17:55 +0800

    _gitconfig: drop setting push.default
    Let the git current in charge do its job
    
    push.default=simple is only available since 1.7.11
    
    OpenWrt host build of git currently has version 1.7.10.4 and complains
    with this "simple" value
    
    - https://github.com/git/git/blob/master/Documentation/RelNotes/1.7.11.txt

--- scripts/git.sh
+++ scripts/git.sh
@@ -6,18 +6,6 @@ config() {
 	local v
 
 	cp "$DATA_DIR/_gitconfig" "$HOME/.gitconfig"
-
-	# Debian wheezy may have a 4th digit as patch level.
-	v="$(git --version | cut -f 3 -d ' ')"
-	v="$(echo "$v" | cut -f 1,2,3 -d . | tr . ' ')"
-	v="$(printf "%02d%02d%02d" $v)"
-
-	# push.default=simple is only available since 1.7.11
-	#  - https://github.com/git/git/blob/master/Documentation/RelNotes/1.7.11.txt
-	if [ "$v" -lt "010711" ]; then
-		# unsetting an non-existing option will exit with 5.
-		git config --global --unset push.default || true
-	fi
 }
 
 collect() {
