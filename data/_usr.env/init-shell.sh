812c0c4848698feeba17dfe5ad21c44f936d012e
Yousong Zhou <yszhou4tech@gmail.com>
2015-12-18 11:13:39 +0800

    init-shell.sh: add colorful man
    This was taken from link [1] with background color for standout
    (terminfo cap so, se).
    
    Related manpage are terminfo (5), tput (1), and link [2]
    
    Some commands
    
        for i in $(seq 1 1024); do tput setab $i; echo -n "$i hello "; done
        for i in $(seq 1 1024); do tput setaf $i; echo -n "$i hello "; done
    
    [1] https://wiki.archlinux.org/index.php/Man_page#Colored_man_pages
    [2] http://unix.stackexchange.com/questions/108699/documentation-on-less-termcap-variables

--- data/_usr.env/init-shell.sh
+++ data/_usr.env/init-shell.sh
@@ -77,6 +77,7 @@ else
 	export EDITOR=vi
 fi
 
+# colorful ls output
 _init_color() {
 	local colors="$PREFIX_USR_ENV/dircolors-solarized/dircolors.ansi-dark"
 	[ -r "$colors" ] || {
@@ -85,6 +86,18 @@ _init_color() {
 	eval $(dircolors "$colors")
 }
 _init_color
+
+# colorful man page
+man() {
+	env LESS_TERMCAP_mb=$'\E[01;31m' \
+		LESS_TERMCAP_md=$'\E[01;38;5;74m' \
+		LESS_TERMCAP_me=$'\E[0m' \
+		LESS_TERMCAP_se=$'\E[0m' \
+		LESS_TERMCAP_so=$'\E[38;5;246;44m' \
+		LESS_TERMCAP_ue=$'\E[0m' \
+		LESS_TERMCAP_us=$'\E[04;38;5;146m' \
+		man "$@"
+}
 
 if [ -r "$PREFIX_USR_ENV/.env.sh" ]; then
 	. "$PREFIX_USR_ENV/.env.sh"
