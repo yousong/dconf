a77f18fe2ce9aae4b187716adb31eccb0920bb9d
Yousong Zhou <yszhou4tech@gmail.com>
2016-10-18 10:28:57 +0800

    pip.conf: support python pip.conf

---
+++ scripts/pip.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -e
+
+. "$TOPDIR/env.sh"
+
+__pip_conf="$HOME/.config/pip/pip.conf"
+__pip_conf_legacy="$HOME/.pip/pip.conf"
+__pip_conf_macosx="$HOME/Library/Application Support/pip/pip.conf"
+#
+# only use legacy since most of environments I am currently working on only
+# recogise it
+#
+__pip_conf_files="
+	$__pip_conf_legacy
+"
+
+config() {
+	local f dataf
+
+	for f in $__pip_conf_files; do
+		if [ -f "$f" ]; then
+			dataf="${f##$HOME/.}"
+			dataf="$DATA_DIR/_$dataf"
+			mkdir -p "$(dirname "$f")"
+			cp "$dataf" "$f"
+		fi
+	done
+}
+
+collect() {
+	local f dataf
+
+	for f in $__pip_conf_files; do
+		if [ -f "$f" ]; then
+			dataf="${f##$HOME/.}"
+			dataf="$DATA_DIR/_$dataf"
+			mkdir -p "$(dirname "$dataf")"
+			cp "$f" "$dataf"
+		fi
+	done
+}
