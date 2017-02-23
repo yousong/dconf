ae796e73adc18f71bf7118c87904863527976b37
Yousong Zhou <yszhou4tech@gmail.com>
2017-02-23 10:06:58 +0800

    openwrt.plugin.sh: add dl_linux

--- data/_usr.env/openwrt.plugin.sh
+++ data/_usr.env/openwrt.plugin.sh
@@ -17,6 +17,36 @@ openwrt_build_current() {
 	cd ..
 }
 
+dl_linux() {
+	local ver="$1"
+	local oIFS="$IFS"; IFS="."; set -- $ver; IFS="$oIFS"
+	local ver_maj="$1"
+	local ver_min="$2"
+	local subdir fn
+	local u urls
+
+	if [ -z "$ver_maj" -o -z "$ver_min" ]; then
+		__errmsg "dl_linux usage examples:"
+		__errmsg "  dl_linux 1.0"
+		__errmsg "  dl_linux 4.4.49"
+		return 1
+	fi
+	if [ "$ver_maj" -ge 3 ]; then
+		subdir="v$ver_maj.x"
+	else
+		subdir="v$ver_maj.$ver_min"
+	fi
+	fn="linux-$ver.tar.xz"
+	urls="
+http://mirrors.ustc.edu.cn/kernel.org/linux/kernel/$subdir/$fn
+http://mirrors.aliyun.com/linux-kernel/$subdir/$fn
+"
+
+	for u in $urls; do
+		wget -O "$fn" -c "$u" && break
+	done
+}
+
 openwrt_repo_dir=
 
 # Run this from inside OpenWrt/LEDE sub-project repo <currepo>.
