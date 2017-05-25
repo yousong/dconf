6774304790c9cb8de510616d4a1ed183c293440b
Yousong Zhou <yszhou4tech@gmail.com>
2017-05-25 11:26:19 +0800

    go.plugin.sh: automatically select highest available go version

--- data/_usr.env/go.plugin.sh
+++ data/_usr.env/go.plugin.sh
@@ -1,6 +1,4 @@
 # golang
-_gover=1.7.4
-
 _go_path_match() {
 	local dir="$1"
 
@@ -12,17 +10,36 @@ _go_path_match() {
 }
 
 go_select() {
-	local ver="${1:-$_gover}"
+	local ver="$1"
 	local q="$2"
-	local goroot="$PREFIX_USR/go/goroot-$ver"
-	local d
+	local goroot
+	local ver
 
+	if [ -z "$ver" ]; then
+		goroot="$(echo "$PREFIX_USR/go/goroot-"*)"
+		goroot="${goroot##* }"
+		ver="${goroot#*-}"
+	else
+		goroot="$PREFIX_USR/go/goroot-$ver"
+	fi
 	if [ ! -d "$goroot" ]; then
 		if [ -z "$q" ]; then
 			__errmsg "$goroot does not exist"
 		fi
 		return 1
 	fi
+	# https://golang.org/doc/go1compat
+	#
+	#	> Compatibility is at the source level. Binary compatibility for compiled
+	#	> packages is not guaranteed between releases. After a point release, Go
+	#	> source will need to be recompiled to link against the new release.
+	#
+	# https://groups.google.com/forum/#!topic/golang-nuts/TUyhyFeX0ig
+	#
+	#	> There is no compatibility for compiled packages between different
+	#	> versions of go
+	#
+	# That's why $GOPATH needs to have a full $ver spec, not only part of it
 	export GOROOT="$PREFIX_USR/go/goroot-$ver"
 	export GOPATH="$PREFIX_USR/go/gopath-$ver"
 	path_ignore_match PATH _go_path_match
