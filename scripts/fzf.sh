6b2eceedf754d1ddca23b88077ddc0f5e3374409
Yousong Zhou <yszhou4tech@gmail.com>
2019-05-11 02:09:53 +0800

    scripts/fzf.sh: fallback to download prebuilt binary

--- scripts/fzf.sh
+++ scripts/fzf.sh
@@ -2,11 +2,26 @@
 
 . "$TOPDIR/env.sh"
 
+fzf_ver=0.18.0
 fzf_dir="$o_homedir/.fzf"
 
 config() {
 	[ -d "$fzf_dir" ] || {
-		go get -u github.com/junegunn/fzf
+		go get -u github.com/junegunn/fzf || {
+			local f url
+			case "$o_os" in
+				Darwin) f="fzf-$fzf_ver-darwin_amd64.tgz" ;;
+				Linux) f="fzf-$fzf_ver-linux_amd64.tgz" ;;
+				*) __error "unknown os $o_os"; false; ;;
+			esac
+			url="https://github.com/junegunn/fzf-bin/releases/download/$fzf_ver/$f"
+
+			__warning "downloading $f"
+			wget -O /tmp/fzf.tgz "$url"
+			mkdir -p "$HOME/.usr/bin"
+			tar -xzf /tmp/fzf.tgz -C "$HOME/.usr/bin/"
+			rm -f /tmp/fzf/tgz
+		}
 		git clone https://github.com/junegunn/fzf.git "$fzf_dir"
 	}
 }
