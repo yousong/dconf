ca5b10ead2e53218d25806ec1983b40e77a03c5e
Yousong Zhou <yszhou4tech@gmail.com>
2020-04-17 11:37:16 +0800

    _vimrc.plugins: remove commented-out completer plugins

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -4,8 +4,6 @@
 
 bundle_dir="$o_homedir/.vim/bundle"
 
-__vim_coc_nvim_bundle="$bundle_dir/coc.nvim"
-
 __vimrc_files="
 	$o_homedir/.vimrc
 	$o_homedir/.vimrc.plugins
@@ -14,47 +12,6 @@ __vimrc_files="
 
 __vim_foreach_patchdir() {
 	find "$PATCH_DIR/_vim" -mindepth 1 -maxdepth 1 -type d 2>/dev/null
-}
-
-__vim_has_node() {
-	which node &>/dev/null
-}
-
-__vim_handle_coc() {
-	if [ ! -d "$bundle_dir/coc.nvim" ]; then
-		return
-	fi
-
-	# It's written in typescript, javascript code available in
-	# "release" branch
-	git \
-		--git-dir "$bundle_dir/coc.nvim/.git" \
-		--work-tree "$bundle_dir/coc.nvim" \
-		checkout refs/remotes/origin/release
-	# Extensions are placed under $o_homedir/.config/coc/
-	#
-	# "-sync" means waiting for the command finish
-	#vim -c 'CocInstall -sync coc-tsserver coc-json | q'
-
-	# Update & uninstall extensions
-	#CocUpdateSync
-	#CocUninstall coc-tsserver
-	# Edit config ~/.vim/coc-settings.json
-	#CocConfig
-
-	# TODO
-	#
-	#  - How it works?
-	#  - luci.js and friends
-	#  - golang packages
-	#
-	# Languages, https://github.com/neoclide/coc.nvim/wiki/Language-servers
-	#
-	#  - c/c++/objective-c: ccls
-	#    - build big c++ project
-	#    - per-project ccls settings
-	#  - js/ts: coc-tsserver extension
-	#    - how to use it?
 }
 
 __vim_handle_ycm() {
@@ -126,16 +83,9 @@ config() {
 			cp "$dataf" "$f"
 		fi
 	done
-	if ! __vim_has_node; then
-		# It requires nodejs (>= 8.10.0)
-		__notice "disable coc.nvim because nodejs is not found"
-		sed -i -e '/neoclide\/coc.nvim/d' "$o_homedir/.vimrc.plugins"
-	fi
 
 	__notice "vim: Installing Vundle packages, this may take a while."
 	vim +BundleInstall +qa
-
-	__vim_handle_coc
 
 	for patchdir in `__vim_foreach_patchdir`; do
 		name="${patchdir##*/}"
