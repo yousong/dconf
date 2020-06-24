cfd3641ac92b8343dc72ee559839c28247f15823
Yousong Zhou <yszhou4tech@gmail.com>
2020-06-24 17:54:10 +0800

    distver.plugin.sh: go_select: GO111MODULE: leave it alone

--- data/_usr.env/distver.plugin.sh
+++ data/_usr.env/distver.plugin.sh
@@ -106,7 +106,6 @@ go_select() {
 	if which go &>/dev/null; then
 		export GOPROXY=https://mirrors.aliyun.com/goproxy/
 		export GOPATH="$HOME/go"
-		unset GO111MODULE
 		if [ -z "$GOROOT" -o ! -d "$GOROOT" ]; then
 			unset GOROOT
 			# go installed by package managers
