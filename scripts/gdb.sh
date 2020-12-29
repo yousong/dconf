de72012cd7626416978c6ca7dfe5e063f9c355cd
Yousong Zhou <yszhou4tech@gmail.com>
2020-12-29 20:26:21 +0800

    gdb.sh: include .gdbinit

---
+++ scripts/gdb.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+. "$TOPDIR/env.sh"
+
+config() {
+	cp "$DATA_DIR/_gdbinit" "$o_homedir/.gdbinit"
+}
+
+collect() {
+	if [ -f "$o_homedir/.gdbinit" ]; then
+		cp "$o_homedir/.gdbinit" "$DATA_DIR/_gdbinit"
+	fi
+}
