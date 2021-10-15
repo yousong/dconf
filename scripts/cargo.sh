06ecfbe132783756a1c948df9269f96c6295800d
Yousong Zhou <yszhou4tech@gmail.com>
2021-10-15 11:13:55 +0800

    _cargo: include rust cargo config

---
+++ scripts/cargo.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+. "$TOPDIR/env.sh"
+
+cargo_conf_dir="$o_homedir/.cargo"
+cargo_conf="$o_homedir/.cargo/config"
+
+config() {
+	mkdir -p "$cargo_conf_dir"
+	chmod 700 "$cargo_conf_dir"
+	cp "$DATA_DIR/_cargo/config" "$cargo_conf"
+	chmod 600 "$cargo_conf"
+}
+
+collect() {
+	if [ -f "$cargo_conf" ]; then
+		mkdir -p "$DATA_DIR/_cargo"
+		cp "$cargo_conf" "$DATA_DIR/_cargo/config"
+	fi
+}
