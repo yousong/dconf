d19aa24764391b24febd335e00a3915df602a186
Yousong Zhou <yous.zhouys@alibaba-inc.com>
2025-08-21 13:41:10 +0800

    Notes.sh: initial version

---
+++ scripts/Notes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+. "$TOPDIR/env.sh"
+
+Notes_dir="$o_homedir/Library/Group Containers/group.com.apple.notes"
+Notes_db="$Notes_dir/NoteStore.sqlite"
+
+config_private_data() {
+	__config_private_data \
+		Notes \
+		"$Notes_db" \
+		"$DATA_PRIVATE_DIR/Notes/${Notes_db##*/}"
+}
+
+collect_private_data() {
+	__collect_private_data \
+		Notes \
+		"$Notes_db" \
+		"$DATA_PRIVATE_DIR/Notes/${Notes_db##*/}"
+}
